c72503769cdb906a9f7bcce1252a964ab93dae38
sergey pilyugi <krokojabba@gmail.com>
2023-01-01 22:45:47 +0300

    fix bug

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,6 @@ import readlineSync from "readline-sync"; //библиотека для чтен
 export default () => {
   console.log("Welcome to the Brain Games!");
   const userName = readlineSync.question("May I have your name? "); //Пользователь вводит имя
-  console.log(`Hello,  ${userName}!`); //Приветствуем пользователя
+  console.log(`Hello, ${userName}!`); //Приветствуем пользователя
   return userName;
 };
